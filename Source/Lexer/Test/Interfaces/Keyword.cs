fea71b8c850832d92c2b3f75181e3ce36dabea0f
amin karvizi <aminkarvizi1384@gmail.com>
2022-02-13 11:50:59 +0330

    Added FLT

--- Source/Lexer/Test/Interfaces/Keyword.cs
+++ Source/Lexer/Test/Interfaces/Keyword.cs
@@ -3,7 +3,7 @@ using System.Text.RegularExpressions;
 
 namespace MiMSharp.Lang.Lexer.Test.LexerTest.Interfaces
 {
-    public class Identifiers
+    public class Keywords
     {
         public const string defNumber = "decn";
         public const string defString = "decstr";
--- Source/Lexer/Test/Program.cs
+++ Source/Lexer/Test/Program.cs
@@ -7,72 +7,113 @@ using System.Text.RegularExpressions;
 namespace MiMSharp.Lang.Lexer.Test.LexerTest
 {
 
-    class Program
+    public class Program
     {
-        static StreamReader fr;
+
         public static void Main(string[] args)
         {
+            Lexer lexer = new Lexer(Path.Combine(AppContext.BaseDirectory, "Test.txt"));
+            IEnumerable<Tuple<string, string, int, int>> TokensizesList = lexer.Lex();
 
-            FileStream fs = new FileStream(Path.Combine(AppContext.BaseDirectory, "Test.txt"), FileMode.Open);
-            using (fr = new StreamReader(fs))
+            foreach (Tuple<string, string, int, int> item in TokensizesList)
             {
-                List<Tuple<string,string>> Ids = new List<Tuple<string,string>>();
-                List<Tuple<string,string>> Opereators  = new List<Tuple<string,string>>();
-                List<Tuple<string,int>> Numbers = new List<Tuple<string,int>>();
-                char readedChar = (char)fr.Read();
-                while (readedChar != '\uffff')
-                {
-                    switch (readedChar)
-                    {
-                        //For keywords
-                        case char c when Regex.IsMatch(c.ToString(), "[a-zA-Z]"):
-                            string word = default;
-                            while (Regex.IsMatch(readedChar.ToString(), "[a-zA-Z]"))
-                            {
-                                word += readedChar;
-                                readedChar = (char)fr.Read();
-                            }
-                            Ids.Add(new Tuple<string, string>("ID",word));
-                            break;
-                        //For opertators
-                        case char c when Regex.IsMatch(c.ToString(), "[+=*/-]"):
-                            Opereators.Add(new Tuple<string, string>("OP",c.ToString()));
-                            readedChar = (char)fr.Read();
-                            break;
-                        //For numbers
-                        case char c when Regex.IsMatch(c.ToString(), "[0-9]"):
-                            string number = default;
-                            while (Regex.IsMatch(readedChar.ToString(), "[0-9]"))
-                            {
-                                number += readedChar;
-                                readedChar = (char)fr.Read();
-                            }
-                            Numbers.Add(new Tuple<string, int>("NUM",int.Parse(number)));
-                            break;
-                        default:
-                            readedChar = (char)fr.Read();
-                            break;
-                    }
-                }
-                Console.ForegroundColor = ConsoleColor.Blue;
-                foreach (var item in Ids)
-                {
-                    System.Console.WriteLine(item.Item1 +" : "+item.Item2);
-                }
-                Console.ForegroundColor = ConsoleColor.Green;
-                foreach (var item in Opereators)
-                {
-                    System.Console.WriteLine(item.Item1 +" : "+item.Item2);
-                }
-                Console.ForegroundColor = ConsoleColor.Red;
-                foreach (var item in Numbers)
-                {
-                    System.Console.WriteLine(item.Item1 +" : "+item.Item2);
-                }
+                Console.WriteLine($"{item.Item1} => {item.Item2} at {item.Item3}:{item.Item4}");
             }
         }
-        // public static bool IsIdentifier(string word) =>
-        //     word == Identifiers.defNumber ||
-        //     word == Identifiers.defString;
+    }
+
+    public class Lexer
+    {
+        private string _inputStream;
+        private static List<Tuple<string, string, int, int>> TokensizesList = new List<Tuple<string, string, int, int>>();
+        public Lexer(string inputStream)
+        {
+            _inputStream = inputStream;
+        }
+        public IEnumerable<Tuple<string, string, int, int>> Lex()
+        {
+            using (FileStream fs = new FileStream(_inputStream, FileMode.Open))
+            {
+                using (StreamReader fr = new StreamReader(fs))
+                {
+                    char readedChar = (char)fr.Read();
+                    int line = 1;
+                    int lastcol = 1;
+                    while (readedChar != '\uffff')
+                    {
+                        if (readedChar == '\n')
+                        {
+
+                            line++;
+                            lastcol = 1;
+                            readedChar = (char)fr.Read();
+                        }
+                        switch (readedChar)
+                        {
+                            //For keywords
+                            case char c when Regex.IsMatch(c.ToString(), "[a-zA-Z]"):
+                                string word = default;
+                                while (Regex.IsMatch(readedChar.ToString(), "[a-zA-Z]"))
+                                {
+                                    word += readedChar;
+                                    readedChar = (char)fr.Read();
+                                }
+
+                                if (IsKeyword(word))
+                                    TokensizesList.Add(new Tuple<string, string, int, int>("KW_VAR", word, line, lastcol));
+                                else
+                                    TokensizesList.Add(new Tuple<string, string, int, int>("ID", word, line, lastcol));
+
+                                lastcol += word.Length;
+                                break;
+                            //For opertators
+                            case char c when Regex.IsMatch(c.ToString(), "[+=*/-]"):
+                                TokensizesList.Add(new Tuple<string, string, int, int>("OP", c.ToString(), line, lastcol));
+                                lastcol += c.ToString().Length;
+                                readedChar = (char)fr.Read();
+                                break;
+                            //For numbers
+                            case char c when Regex.IsMatch(c.ToString(), "[0-9]"):
+                                string number = default;
+                                while (Regex.IsMatch(readedChar.ToString(), "[0-9]") || readedChar == '.')
+                                {
+                                    number += readedChar;
+                                    readedChar = (char)fr.Read();
+                                }
+                                if (Regex.IsMatch(number, @"[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)") && number.Contains("."))
+                                {
+                                    TokensizesList.Add(new Tuple<string, string, int, int>("FLT", float.Parse(number).ToString(), line, lastcol));
+                                }
+                                else if(Regex.IsMatch(number,"[0-9]"))
+                                {
+                                    TokensizesList.Add(new Tuple<string, string, int, int>("NUM", number, line, lastcol));
+                                }
+                                else{
+                                    throw new Exception($"Unknown number type at {line}:{lastcol} ");
+                                }
+                                lastcol += number.Length;
+                                break;
+                            case ' ':
+                                readedChar = (char)fr.Read();
+                                lastcol += 1;
+                                break;
+
+                            default:
+                                if (readedChar != '\r')
+                                    throw new Exception($"Unknown syntax at {line}:{lastcol} ");
+                                else
+                                    readedChar = (char)fr.Read();
+                                break;
+                        }
+                    }
+                }
+            }
+
+            return TokensizesList;
+        }
+
+        public bool IsKeyword(string word) =>
+    word == Keywords.defNumber ||
+    word == Keywords.defString;
     }
 }
